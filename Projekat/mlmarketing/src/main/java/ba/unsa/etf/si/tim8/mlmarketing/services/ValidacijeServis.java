8aea610e3d31a6feb7ad548695b7565aaedb2e51
Amir Lisovac <alisovac1@etf.unsa.ba>
2016-05-14 16:36:25 +0200

    ValidacijaServis: ime, prezime, email, daLiJeInt, daLiJeDouble

--- Projekat/mlmarketing/src/main/java/ba/unsa/etf/si/tim8/mlmarketing/services/ValidacijeServis.java
+++ Projekat/mlmarketing/src/main/java/ba/unsa/etf/si/tim8/mlmarketing/services/ValidacijeServis.java
@@ -3,14 +3,42 @@ package ba.unsa.etf.si.tim8.mlmarketing.services;
 public class ValidacijeServis {
 	
 	public static boolean  validirajIme(String ime){
-		String pattern = "[a-zA-Z]"; 
-		
-		return ime.matches(pattern);
-		
-		
+		String pattern = "[a-zA-ZšŠćĆĐđŽžČč]+"; 		
+		return ime.matches(pattern);		
 	}
 	
-	/*boolean validirajPrezime(String prezime){
-		
-	}*/
+	public static boolean validirajPrezime(String prezime){
+		String pattern = "[a-zA-ZšŠćĆĐđŽžČč]+"; 		
+		return prezime.matches(pattern);
+	}
+	
+	public static boolean daLiJeInt(String broj)
+	{
+		int duzina = broj.length();		
+		if(broj == null || duzina == 0)
+			return false;
+		for(int i = 0; i < duzina; i++)
+		{
+			char c = broj.charAt(i);
+			if(c < '0' || c > '9')
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean daLiJeDouble(String broj)
+	{
+		if(daLiJeInt(broj)) return true;
+		String pattern = "([0-9]*)\\.([0-9]*)";
+		return broj.matches(pattern);
+	}
+	
+	public static boolean validirajEmail(String email)
+	{
+		String pattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+		        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		return email.matches(pattern);
+	}
 }
