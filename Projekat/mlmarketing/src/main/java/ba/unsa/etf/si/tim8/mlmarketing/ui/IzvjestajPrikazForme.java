1067261be93ffe79019c521d1151a175fb4cf268
Amir Lisovac <alisovac1@etf.unsa.ba>
2016-05-15 16:22:58 +0200

    Gui za prikazivanje pojedinacnih izvjestaja

---
+++ Projekat/mlmarketing/src/main/java/ba/unsa/etf/si/tim8/mlmarketing/ui/IzvjestajPrikazForme.java
@@ -0,0 +1,69 @@
+package ba.unsa.etf.si.tim8.mlmarketing.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import org.hibernate.Session;
+
+import javax.swing.JButton;
+
+public class IzvjestajPrikazForme {
+
+	private JFrame frmPrikazIzvjetaja;
+	private JTable table;
+	private MyTableModel m;
+	private Session s;
+
+	/**
+	 * Launch the application.
+	 */
+	public  void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IzvjestajPrikazForme window = new IzvjestajPrikazForme(m);
+					window.frmPrikazIzvjetaja.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public IzvjestajPrikazForme(MyTableModel m) {
+		initialize();
+		this.m = m;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPrikazIzvjetaja = new JFrame();
+		frmPrikazIzvjetaja.setTitle("Prikaz izvještaja");
+		frmPrikazIzvjetaja.setBounds(100, 100, 450, 300);
+		frmPrikazIzvjetaja.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPrikazIzvjetaja.getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 414, 188);
+		frmPrikazIzvjetaja.getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton btnSacuvaj = new JButton("Sačuvaj");
+		btnSacuvaj.setBounds(335, 227, 89, 23);
+		frmPrikazIzvjetaja.getContentPane().add(btnSacuvaj);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setBounds(236, 227, 89, 23);
+		frmPrikazIzvjetaja.getContentPane().add(btnOk);
+	}
+}
