60ca95675d59c59d5409b520297e0417aeb6498e
Adi Palavra <apalavra1@etf.unsa.ba>
2016-05-22 19:55:01 +0200

    Dodani db.properties, promjene na pom.xml i hibernate.cfg.xml zbog exporta u jar

--- Projekat/mlmarketing/src/main/java/ba/unsa/etf/si/tim8/mlmarketing/ui/HibernateUtil.java
+++ Projekat/mlmarketing/src/main/java/ba/unsa/etf/si/tim8/mlmarketing/ui/HibernateUtil.java
@@ -2,6 +2,12 @@ package ba.unsa.etf.si.tim8.mlmarketing.ui;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.imageio.spi.ServiceRegistry;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
 
@@ -10,7 +16,9 @@ public class HibernateUtil {
 	    private static SessionFactory buildSessionFactory() {
 	        try {
 	            // Create the SessionFactory from hibernate.cfg.xml
-	            return new Configuration().configure().buildSessionFactory();
+	        	java.util.Properties properties = new Properties();
+	        	properties.load(new FileInputStream("db.properties"));
+	        	return new Configuration().addProperties(properties).configure("hibernate.cfg.xml").buildSessionFactory();
 	        }
 	        catch (Exception ex) { //Exception ili Throwable ?
 	            // Make sure you log the exception, as it might be swallowed
